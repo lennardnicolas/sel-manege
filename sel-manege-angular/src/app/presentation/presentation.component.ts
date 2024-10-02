ce434938b1c07db067ad4f5cdaaf7445b7c9846b
Nicolas Lennard <nicolas@lennard.swiss>
2024-10-02 21:26:25 +0200

    Fix contacte-nous link on presentation

--- sel-manege-angular/src/app/presentation/presentation.component.ts
+++ sel-manege-angular/src/app/presentation/presentation.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../navbar/navbar.component'
 import { FooterComponent } from '../footer/footer.component'
 import { MatDividerModule } from '@angular/material/divider'
 import { MatButtonModule } from '@angular/material/button'
+import { RouterModule } from '@angular/router'
 
 @Component({
     selector: 'app-presentation',
     standalone: true,
-    imports: [NavbarComponent, FooterComponent, MatDividerModule, MatButtonModule],
+    imports: [NavbarComponent, FooterComponent, MatDividerModule, MatButtonModule, RouterModule],
     templateUrl: './presentation.component.html',
     styleUrl: './presentation.component.css',
 })
